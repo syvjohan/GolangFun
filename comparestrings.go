2c0b93ad70cee42ac69e18ed6031aa3546ca2c9e
syvjohan <johanfredriksson1987@gmail.com>
2014-03-15 12:52:30 +0100

    comparing strings

---
+++ comparestrings.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func strcmp(a, b string) int {
+  var min = len(b)
+  if len(a) < len(b) {
+    min = len(a)
+  }
+
+  var diff int
+  for i := 0; i < min && diff == 0; i++ {
+    diff = int(a[i]) - int(b[i])
+  }
+  if diff == 0 {
+    diff = len(a) - len(b)
+  }
+  return diff
+}
